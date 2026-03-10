155658832975abdfc402674ceae7f9fc8d722091
himanshu-sharma27 <himanshu.madduri@gmail.com>
2026-03-10 15:41:12 +0530

    React axios

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,15 @@ import Header from "./components/Header";
 
 function App() {
   return (
-    <div>
+    <>
       <Header />
       <Content />
       <Footer />
-    </div>
+    </>
     
   )
 
 }
-
-
-
-
 
 
 export default App;
