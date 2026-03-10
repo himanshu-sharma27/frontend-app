9262c443f2ce1613d5e5fbc57b5b9bebe6005849
himanshu-sharma27 <himanshu.madduri@gmail.com>
2026-03-10 15:01:56 +0530

    basic react ui

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import Content from "./components/Content";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Content />
+      <Footer />
+    </div>
+    
+  )
+
+}
+
+
+
+
+
+
+export default App;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import './Footer.css';
+
+function Footer () {
+    return (
+        <div className = "App-Footer">
+            <h3>&copy; 2026. All rights reserved</h3>
+        </div>
+    )
+}
+
+export default Footer;
