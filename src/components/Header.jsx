68c15c4a5ec49569ed5abe1faf0dcad681d30d9a
himanshu-sharma27 <himanshu.madduri@gmail.com>
2026-03-11 14:07:51 +0530

    Added routing to all components

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,19 @@
 import "./Header.css";
+import { Link } from "react-router-dom";
 
 function Header() {
-    return <div className="App-Header"> 
-        <h1>My Application</h1>
-        <ul>
-            <li>Home</li>
-            <li>Cart</li>
-            <li>Orders</li>
-            <li>Login</li>
-            <li>Logout</li>
-        </ul>
+  return (
+    <div className="App-Header">
+      <h1>My Application</h1>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/cart">Cart</Link></li>
+        <li><Link to="/orders">Orders</Link></li>
+        <li><Link to="/login">Login</Link></li>
+        <li><Link to="/logout">Logout</Link></li>
+      </ul>
     </div>
+  );
 }
 
 export default Header;
